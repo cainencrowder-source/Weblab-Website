7b2e5fb580a0e552b4cc6009e531b69a652cdaa4
Cainen Crowder <cainencrowder@lawn-143-215-92-34.lawn.gatech.edu>
2025-09-25 23:33:04 -0400

    init

---
+++ Home_Page.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section E")
+st.subheader("Cainen Crowder")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to my Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **About Me**:This "About Me" page serves as my digital professional introduction. The page opens with a little bit of my story, I’m a civil engineering student at Georgia Tech with a strong background in real estate, public sector engineering, and music direction. Outside of academics, I’m passionate about wellness, performance, and creating meaningful experiences through music, leadership, and community involvement.
+2. **Music Quiz**: This quiz takes a part of me and shares it with you, lets see your knowledge of some basic music question and see if your vibes really are that good!
+
+
+""")
+
---
+++ info.py
@@ -0,0 +1,83 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "/Users/cainencrowder/Downloads/WebDevLab01 2/WebDevLab01/Images/Cainen Headshot.jpeg"
+about_me = "I'm Cainen Crowder, an Industrial Engineering student, here at Georgia Tech. I have a strong interest in civil engineering, consulting, and sustainable development. I am originally from Cartersville, Georgia, and my background combines academic study with hands-on experience in both the public and private sectors. I have worked with the Bartow County Engineering Department on infrastructure and stormwater projects, as well as in real estate management and appraisals. Beyond engineering, I serve as Music Director for local theatre productions and am active in campus life through WREK Radio and Delta Chi fraternity, where I hold leadership roles. My goal is to bridge technical problem-solving with creativity, teamwork, and community impact. . "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://logowik.com/content/uploads/images/329_linkedin.jpg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/cainen-crowder/"
+my_github_url = "https://github.com/cainencrowder-source"
+my_email_address = "cainen.crowder@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Civil Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Spring 2028',
+    'GPA': '3.55'
+}
+course_data = {
+    "code":["CS 1301", "PHYS 2211", "MATH 2551", "MUSI 3450"], 
+    "names":["Intro to CS", "Physics 1", "Mulivariable Calculus", "Survery of Music Technology"], 
+    "semester_taken":["3rd", "3rd", "3rd", "3rd"],
+    "skills":["Python Fundamentals", "learning the science behind throwing a ball", "Thinking in 3-D, but with MATH", "How to make songs 101"],
+    }
+experience_data = {
+    "Bartow County Engineering Department": (
+        [  
+            "- Worked as the Everything intern for land development and stormwater management permitting as well as green infrastructure marketing.",
+            "- Increased AutoCad skills by ten fold.",
+            "- Learned the in's and out's of the public sector."
+        ],
+        "/Users/cainencrowder/Downloads/WebDevLab01 2/WebDevLab01/Images/download.jpg"  # <--- ITEM 2: The image path
+    ),
+    
+}
+
+projects_data = {
+"Undergraduate Consulting Club Pro-Bono-Project": "Applied analytical and teamwork skills with Spike Aerospace and Piedmont Park Conservancy, delivering strategies that identified 6,800+ potential volunteers, new revenue streams, and scalable programs for LT growth."
+}                                                                                                                                                                                                                                                                           
+
+programming_data = {
+    "Python": 90,
+    "Java": 20,
+    "C": 10,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Not Fluent",
+    "Spanish": "Introduced to foundations in spanish",
+}
+leadership_data = {
+"Health and Wellness Chair/Co-Homecoming Chair: Delta Chi Fraternity": ( 
+    [ 
+        "- Organized wellness initiatives and internal challenges for member competition to promote fraternity health"
+    ], 
+    "/Users/cainencrowder/Downloads/WebDevLab01 2/WebDevLab01/Images/Wellness.png"
+)
+}
+activity_data={
+"Theme Park Engineering and Design Club": (["- Co-led early concept development for the 2024 escape room, collaborating with team members to generate 5+ adaptive theme-park experiences grounded in industry trends"],
+                                             '/Users/cainencrowder/Downloads/WebDevLab01 2/WebDevLab01/Images/GTTPED.jpg'),
+}
